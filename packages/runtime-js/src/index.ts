1005465c7f9ce5b3ab37b12eb3d318ab91439703
Rory Quinlan <roryquinlan@macbookpro.mynetworksettings.com>
2026-05-16 13:15:08 -0400

    feat(04-01): expose safe runtime api

--- packages/runtime-js/src/index.ts
+++ packages/runtime-js/src/index.ts
@@ -1,1 +1,7 @@
 export const runtimeJsPackage = "@cowards/runtime-js"
+export const RUNTIME_JS_NAME = "runtime-js"
+
+export type {
+  StrategyRevision,
+  StrategyRevisionValidationReport,
+} from "@cowards/spec"
